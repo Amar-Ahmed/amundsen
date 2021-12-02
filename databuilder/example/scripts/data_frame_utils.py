a8095f2049b8bb13a254e91cd73a24703798f83c
David Herrera <dherrera@npss-inc.com>
2021-12-02 13:40:39 -0500

    Update databuilder to pull data asset title from A1 cell in the excel file Files changed: data_frame_utils.py: Add the data asset title in the constructor Refactoring code when the data base attributes are created Add the title in the schema description data_prep.py: Get the data asset title from the contributor file's cell A1 domain.py: Remove the 'hive' prefix from the schema name file_utils.py: Extract the contributor's name from the files's cell A1 the file names is formed by the contributor name and the date (YYYY_MM_DDTHHMM) .xlsx

--- databuilder/example/scripts/data_frame_utils.py
+++ databuilder/example/scripts/data_frame_utils.py
@@ -2,6 +2,7 @@ import sys
 import pandas as pd
 import datetime
 from files_utils import File_Utils
+from typing import Optional
 
 """
     This class contains all the methos that are necessary
@@ -15,12 +16,14 @@ class DataFrameCSV():
     load_file_backup_dir: str
     contributor_name: str
     worksheet_name: str
+    data_asset_title: str
 
-    def __init__(self, sample_data_dir: str, load_file_backup_dir: str, worksheet_name: str, contributor_name: str):
+    def __init__(self, sample_data_dir: str, load_file_backup_dir: str, worksheet_name: str, contributor_name: str, data_asset_title: Optional[str] = None):
         self.worksheet_name = worksheet_name
         self.sample_data_dir = sample_data_dir
         self.load_file_backup_dir = load_file_backup_dir
         self.contributor_name = contributor_name
+        self.data_asset_title= data_asset_title
 
 
     def create_dataframe(self, excel_path: str) -> pd.DataFrame:
@@ -44,7 +47,12 @@ class DataFrameCSV():
         self.data_frame.to_csv(load_file_path,encoding='utf-8',index=False)
         return self.data_frame
 
-    
+    def set_database_attributes(self):
+        # create database, cluster, schema columns
+        self.data_frame['database']= "hive"
+        self.data_frame['cluster']= "gold"
+        self.data_frame['schema']= str(self.contributor_name).lower()
+
     def build_dataframe(self):
         pass
 
@@ -55,19 +63,13 @@ class DataFrameCSV():
 """
 class DataFrameTable(DataFrameCSV):
 
-    def __init__(self, sample_data_dir: str, load_file_backup_dir: str, worksheet_name: str, contributor_name: str):
-        super().__init__(sample_data_dir, load_file_backup_dir, worksheet_name, contributor_name)
-
     def build_data_frame(self) -> pd.DataFrame:
         # check for null is_view values
         self.data_frame['is_view'].fillna('table',inplace=True)
         self.data_frame['name']= [x.lower() for x in self.data_frame['name']]
         # check for missing tags
         self.data_frame= File_Utils.check_tags(self.data_frame)
-        # create database, cluster, schema columns
-        self.data_frame['database']= "hive"
-        self.data_frame['cluster']= "gold"
-        self.data_frame['schema']= "hive_" + str(self.contributor_name).lower()
+        self.set_database_attributes()
         return self.data_frame
 
 
@@ -80,10 +82,7 @@ class DataFrameColumn(DataFrameCSV):
         self.data_frame['is_view'].fillna('table',inplace=True)
         self.data_frame['table_name']=[x.lower() for x in self.data_frame['table_name']]
         self.data_frame['name']=[x.lower() for x in self.data_frame['name']]
-        # create database, cluster, schema columns
-        self.data_frame['database']="hive"
-        self.data_frame['cluster']="gold"
-        self.data_frame['schema']="hive_" + str(self.contributor_name).lower()
+        self.set_database_attributes()
         # assign position int for sort_order
         self.data_frame['sort_order']=[i for i in range(0,self.data_frame.shape[0])]
 
@@ -108,6 +107,7 @@ class DataFrameDataAsset(DataFrameCSV):
         df_temp['description_source']=[e[1:-1] for e in df_temp['description_source']]
         df_temp['description']=""
         self.data_frame = df_temp
+        self.set_database_attributes()
     
     def get_data_asset_description(self) -> pd.DataFrame:
         return  self.data_asset_description
@@ -121,6 +121,6 @@ class DataFrameSchemaDescription(DataFrameCSV):
         # create an empty data frame
         # and the schema key, schema and description columns
         self.data_frame = pd.DataFrame()
-        self.data_frame['schema_key']=["hive://gold.hive_" + str(self.contributor_name).lower()]
-        self.data_frame['schema']=["hive_" + str(self.contributor_name).lower()]
-        self.data_frame['description']=[data_asset_description]
+        self.data_frame['schema_key']= [f"hive://gold.{str(self.contributor_name).lower()}"]
+        self.data_frame['schema']= [str(self.contributor_name).lower()]
+        self.data_frame['description']= [f"{self.data_asset_title}|{data_asset_description}"]
