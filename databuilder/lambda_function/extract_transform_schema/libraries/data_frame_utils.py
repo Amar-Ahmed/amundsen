ec25f2654c5d5463504f6238c6c3c9728db7e507
David Herrera <dherrera@npss-inc.com>
2021-12-22 09:44:12 -0500

    Update code from lambda function extract_transfor_schema Update the code to get the data asset title from the cell A1 Update the code that fix the vulnerability caused by the package NLTK and replace this for the package spacy Remove the code that not being use (libreries that are use in the load lambda function)

--- databuilder/lambda_function/extract_transform_schema/libraries/data_frame_utils.py
+++ databuilder/lambda_function/extract_transform_schema/libraries/data_frame_utils.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from .files_utils import File_Utils
+from typing import Optional
 import boto3
 """
     This class contains all the methos that are necessary
@@ -10,16 +11,18 @@ import boto3
 
 class DataFrameCSV():
 
-    sample_data_dir: str
+    folder_data_dir: str
     contributor_name: str
     worksheet_name: str
     bucket_name: str
+    data_asset_title: str
 
-    def __init__(self, sample_data_dir: str,  worksheet_name: str, contributor_name: str, bucket_name: str):
+    def __init__(self, folder_data_dir: str,  worksheet_name: str, contributor_name: str, bucket_name: str, data_asset_title: Optional[str] = None):
         self.worksheet_name = worksheet_name
-        self.sample_data_dir = sample_data_dir
+        self.folder_data_dir = folder_data_dir
         self.contributor_name = contributor_name
         self.bucket_name = bucket_name
+        self.data_asset_title= data_asset_title
 
 
     def create_dataframe(self, excel_path: str) -> pd.DataFrame:
@@ -28,28 +31,27 @@ class DataFrameCSV():
         self.data_frame=pd.read_excel(excel_path, self.worksheet_name)
         return self.data_frame
 
+    def set_database_attributes(self):
+        # create database, cluster, schema columns
+        self.data_frame['database']= "hive"
+        self.data_frame['cluster']= "gold"
+        self.data_frame['schema']= str(self.contributor_name).lower()
 
     def create_csv(self, csv_file_name: str) -> pd.DataFrame:
         """ With the new data frame create the CSV file in the folder where will
             be consume for the data loader process
         """
         # write file to sample_data dir
-        sample_data_path = os.path.join(self.sample_data_dir, csv_file_name)
-        print(f"Temp path: {sample_data_path}")
-        self.data_frame.to_csv(sample_data_path,encoding='utf-8',index=False)
+        folder_data_path = os.path.join(self.folder_data_dir, csv_file_name)
+        print(f"Temp path: {folder_data_path}")
+        self.data_frame.to_csv(folder_data_path,encoding='utf-8',index=False)
 
+        # comment this to run locally
         # save the file in S3
         s3 = boto3.resource("s3")
-        bucket = s3.Bucket(self.bucket_name) #.put_object(key=s3_path)
-        bucket.upload_file(sample_data_path, csv_file_name)
-
-
-
-        # # write file to back_up_load_file dir
-        # today = datetime.datetime.today()
-        # date = f"{str(today.month)}_{str(today.day)}_{str(today.year)}"
-        # load_file_path = f"{self.load_file_backup_dir}/{self.contributor_name}_{date}{csv_file_name}"
-        # self.data_frame.to_csv(load_file_path,encoding='utf-8',index=False)
+        se_transform_data_path = f"transform_data/{csv_file_name}"
+        bucket = s3.Bucket(self.bucket_name) 
+        bucket.upload_file(folder_data_path, se_transform_data_path)
         return self.data_frame
 
     
@@ -63,9 +65,6 @@ class DataFrameCSV():
 """
 class DataFrameTable(DataFrameCSV):
 
-    def __init__(self, sample_data_dir: str, worksheet_name: str, contributor_name: str, bucket_name: str):
-        super().__init__(sample_data_dir,  worksheet_name, contributor_name, bucket_name)
-
     def build_data_frame(self) -> pd.DataFrame:
         # check for null is_view values
         self.data_frame['is_view'].fillna('table',inplace=True)
@@ -73,16 +72,11 @@ class DataFrameTable(DataFrameCSV):
         # check for missing tags
         self.data_frame= File_Utils.check_tags(self.data_frame)
         # create database, cluster, schema columns
-        self.data_frame['database']= "hive"
-        self.data_frame['cluster']= "gold"
-        self.data_frame['schema']= "hive_" + str(self.contributor_name).lower()
+        self.set_database_attributes()
         return self.data_frame
 
 
 class DataFrameColumn(DataFrameCSV):
-
-    def __init__(self, sample_data_dir: str, worksheet_name: str, contributor_name: str, bucket_name: str):
-        super().__init__(sample_data_dir,  worksheet_name, contributor_name, bucket_name)
 
     def build_data_frame(self) -> None:
         # check badges
@@ -92,9 +86,7 @@ class DataFrameColumn(DataFrameCSV):
         self.data_frame['table_name']=[x.lower() for x in self.data_frame['table_name']]
         self.data_frame['name']=[x.lower() for x in self.data_frame['name']]
         # create database, cluster, schema columns
-        self.data_frame['database']="hive"
-        self.data_frame['cluster']="gold"
-        self.data_frame['schema']="hive_" + str(self.contributor_name).lower()
+        self.set_database_attributes()
         # assign position int for sort_order
         self.data_frame['sort_order']=[i for i in range(0,self.data_frame.shape[0])]
 
@@ -102,9 +94,6 @@ class DataFrameColumn(DataFrameCSV):
 class DataFrameDataAsset(DataFrameCSV):
 
     data_asset_description: str
-
-    def __init__(self, sample_data_dir: str, worksheet_name: str, contributor_name: str, bucket_name: str):
-        super().__init__(sample_data_dir,  worksheet_name, contributor_name, bucket_name)
 
     def build_data_frame(self, df_tables: pd.DataFrame) -> None:
         # rename column to match load file requirements
@@ -122,6 +111,7 @@ class DataFrameDataAsset(DataFrameCSV):
         df_temp['description_source']=[e[1:-1] for e in df_temp['description_source']]
         df_temp['description']=""
         self.data_frame = df_temp
+        self.set_database_attributes()
     
     def get_data_asset_description(self) -> pd.DataFrame:
         return  self.data_asset_description
@@ -135,6 +125,6 @@ class DataFrameSchemaDescription(DataFrameCSV):
         # create an empty data frame
         # and the schema key, schema and description columns
         self.data_frame = pd.DataFrame()
-        self.data_frame['schema_key']=["hive://gold.hive_" + str(self.contributor_name).lower()]
-        self.data_frame['schema']=["hive_" + str(self.contributor_name).lower()]
-        self.data_frame['description']=[data_asset_description]
+        self.data_frame['schema_key']= [f"hive://gold.{str(self.contributor_name).lower()}"]
+        self.data_frame['schema']= [str(self.contributor_name).lower()]
+        self.data_frame['description']= [f"{self.data_asset_title}|{data_asset_description}"]
